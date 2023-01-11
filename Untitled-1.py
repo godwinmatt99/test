421ec13d088c13df255e9e7c439edffcc14a82b2
godwinmatt99 <122414160+godwinmatt99@users.noreply.github.com>
2023-01-10 19:27:52 -0600

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,10 @@
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+import heliopy.data.wind as wind
+
+starttime = datetime(1995, 1, 30, 2, 0, 0)
+endtime = starttime + timedelta(hours=1)
+
+Bdata = wind.mfi_h0(starttime, endtime)
+
+Bdata.peek(columns=['BGSE_0', 'BGSE_1', 'BGSE_2'])
